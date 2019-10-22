7a454d70127264ad20888f292bc41a770a0242ea
marcio antonio <ciorma7@hotmail.com>
2019-10-21 21:50:50 -0300

    primeiro commit

---
+++ SocialNetwork.Web/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialNetwork.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var data = new Dictionary<string, string>
+                {
+                    { "grant_type", "password"},
+                    { "username", model.Username },
+                    { "password", model.Password}
+                };
+
+                using (var client = new HttpClient())
+                {
+
+                }
+            }
+
+            return View();
+        }
+    }
+}
