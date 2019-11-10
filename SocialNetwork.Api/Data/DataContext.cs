5c7624a8e9da66cb3ed682fb08102ae35ae3b4db
marcio antonio <ciorma7@hotmail.com>
2019-11-10 11:05:55 -0200

    tentado associar perfil a conta de usuario (ainda sem sucesso)

---
+++ SocialNetwork.Api/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using SocialNetwork.Api.Models;
+using SocialNetwork.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SocialNetwork.Api.Data
+{
+    public class MyUser: IdentityUser
+    {
+        public virtual Profile Profile { get; set; }
+    }
+
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext()
+            : base("DefaultConnection")
+        {
+        }
+        public System.Data.Entity.DbSet<Profile> Profile { get; set; }
+    }
+}
