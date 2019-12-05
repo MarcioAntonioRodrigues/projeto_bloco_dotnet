d01bda8c44573cc8790a51ddf9d3d0080ceab264
Marcio Antonio Rodrigues de Souza <marcio.dsouza@al.infnet.edu.br>
2019-12-04 21:48:30 -0300

    Alteracao no controller de post

--- SocialNetwork.Web/Controllers/PostsController.cs
+++ SocialNetwork.Web/Controllers/PostsController.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using SocialNetwork.Web.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -28,18 +34,62 @@ namespace SocialNetwork.Web.Controllers
 
         // POST: Posts/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public async Task<ActionResult> Create(PostViewModel model)
         {
-            try
-            {
-                // TODO: Add insert logic here
+            string access_token = Session["access_token"]?.ToString();
 
-                return RedirectToAction("Index");
-            }
-            catch
+            if (string.IsNullOrEmpty(access_token))
             {
-                return View();
+                return RedirectToAction("Login", "Account", null);
             }
+
+            if (ModelState.IsValid)
+            {
+                using (var client = new HttpClient())
+                {
+                    using (var content = new MultipartFormDataContent())
+                    {
+                        client.BaseAddress = new Uri("http://localhost:24260/");
+                        client.DefaultRequestHeaders.Accept.Clear();
+
+                        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", $"{access_token}");
+
+                        content.Add(new StringContent(JsonConvert.SerializeObject(model)));
+
+                        if (Request.Files.Count > 0)
+                        {
+                            byte[] fileBytes;
+                            using (var inputStream = Request.Files[0].InputStream)
+                            {
+                                var memoryStream = inputStream as MemoryStream;
+                                if (memoryStream == null)
+                                {
+                                    memoryStream = new MemoryStream();
+                                    inputStream.CopyTo(memoryStream);
+                                }
+                                fileBytes = memoryStream.ToArray();
+                            }
+                            var fileContent = new ByteArrayContent(fileBytes);
+                            fileContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment");
+                            fileContent.Headers.ContentDisposition.FileName = Request.Files[0].FileName.Split('\\').Last();
+
+                            content.Add(fileContent);
+                        }
+
+                        var response = await client.PostAsync("/api/Posts", content);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            return RedirectToAction("Index", "Gallery");
+                        }
+                        else
+                        {
+                            return View("Error");
+                        }
+                    }
+                }
+            }
+            return View();
         }
 
         // GET: Posts/Edit/5
